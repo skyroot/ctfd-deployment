0cf2e1f78ae89323f2c738507a5a0957e97f133e
initialshl <initials.hl@live.com>
2018-07-06 16:33:48 +0800

    Fix variable expansion in single quote

--- Deployment_Scripts/start_ctfd_instance.sh
+++ Deployment_Scripts/start_ctfd_instance.sh
@@ -37,7 +37,7 @@ echo "Starting CTFd..."
 
 # Start this CTFd instance with uWSGI
 cd "$hostname"
-uwsgi --plugin python -s /tmp/uwsgi_"$hostname".sock -w 'CTFd:create_app()' --chmod-socket=666 --pidfile /tmp/ctfd_"$hostname".pid --pyargv '--ncl-sio-url http://172.18.178.14:8080 --ncl-team-name \$nclteamname' &>/dev/null &
+uwsgi --plugin python -s /tmp/uwsgi_"$hostname".sock -w 'CTFd:create_app()' --chmod-socket=666 --pidfile /tmp/ctfd_"$hostname".pid --pyargv \'--ncl-sio-url http://172.18.178.14:8080 --ncl-team-name $nclteamname\' &>/dev/null &
 
 
 
